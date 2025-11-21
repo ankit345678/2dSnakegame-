26218bf26e9ed2b4ae585f01027ffbd6943ac432
Ankita Das <142157928+ankit345678@users.noreply.github.com>
2025-11-21 14:11:38 +0530

    Add files via upload

---
+++ snake2d/MyWindow.java
@@ -0,0 +1,25 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class MyWindow {
+
+	public static void main(String[] args) {
+		
+		JFrame f = new JFrame();
+		f.setTitle("Snake Game");
+		Gameplay gameplay = new Gameplay();
+		f.add(gameplay);
+		
+		f.setBounds(10,10,905,700);
+		f.setResizable(false);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setBackground(Color.DARK_GRAY);
+		
+		
+		
+
+	}
+
+}
